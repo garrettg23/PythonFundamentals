8885e5959c5a218f943e1fec72e292c98bccfad8
garrettg23 <garrettg23@gmail.com>
2023-09-11 16:03:14 -0500

    Added in new files to Basics folder.

---
+++ Basics/MultipleAssignment.py
@@ -0,0 +1,16 @@
+# multiple assignment = allows us to assign multiple variables at the same time using one line of code.
+
+name = "Garrett"
+age = 26
+attractive = True
+
+print(name)
+print(age)
+print(attractive)
+
+# Now use one line of code to define these.
+name, age, attractive = "Garrett", 26, True
+
+print(name)
+print(age)
+print(attractive)
---
+++ Basics/StringMethod.py
@@ -0,0 +1,15 @@
+# String Methods Demonstration
+
+name = "garrett"
+
+print(len(name)) # length in characters of the variable
+print(name.find("a")) # finds where a character is in the array of the variable.
+print(name.capitalize()) # Upper case first character
+print(name.upper()) # Upper case the entire variable
+print(name.lower()) # All lower case
+print(name.isdigit()) # Is a digit? Boolean response
+print(name.isalpha()) # Are all chatacters in alphabet? Boolean response
+print(name.count("r")) # Counts the occurences of the letter r in name
+print(name.replace("r","b")) # replaces all r's with b's in name
+print(name*3) # not a method, but prints name 3 times
+print(name.replace("r","b")*3) # quiz time, what does this do?
