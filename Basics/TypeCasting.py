54ac11a7989f5c47b9e7949bc37aa81a4b316dc0
garrettg23 <garrettg23@gmail.com>
2023-09-13 17:03:48 -0500

    Added new file to basics folder

---
+++ Basics/TypeCasting.py
@@ -0,0 +1,16 @@
+# type casting = conver the data type of a value to another data type.
+
+x = 6 #integer
+y = 2.0 #float
+z = "3" #string
+
+x = str(x) # converts the integer to string
+y = int(y) # converts the floating point number to a integer
+z = float(z) # converts the string to a float
+
+print("The value of the variable x is "+x)
+print(y*y)
+print(z*3)
+
+# You can also do this directly in the print statement, but this is not permanent to the variable.
+print(int(x)*5)
